60d3a15442807dfb6403a3c14dee5acf033081e9
Tejas Birla <tejasbirla3@gmail.com>
2025-04-12 15:24:14 +0530

    Breadcrum's CSS updated.

--- frontend/src/Components/Breadcrums/Breadcrum.jsx
+++ frontend/src/Components/Breadcrums/Breadcrum.jsx
@@ -6,7 +6,6 @@ export default function Breadcrum(props) {
   const { product } = props;
   return (
     <div className="breadcrum">
-      HOME <img src={arrowIcon} alt="" />
       SHOP <img src={arrowIcon} alt="" />
       {product.category} <img src={arrowIcon} alt="" />
       {product.name}
