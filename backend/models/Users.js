f67aa077f364d415cea369475d49b356ba30e044
Tejas Birla <tejasbirla3@gmail.com>
2025-08-03 17:09:54 +0530

    cartData field updated

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -4,10 +4,8 @@ const Users = mongoose.model("Users", {
   name: { type: String },
   email: { type: String, unique: true },
   password: { type: String },
-  cartData: { type: Object },
+  cartData: { type: Object, default: {} },
   Date: { type: Date, default: Date.now() },
 });
 
 export default Users;
-
-
