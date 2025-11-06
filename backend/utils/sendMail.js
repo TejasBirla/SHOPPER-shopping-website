6da07e9d28e979020948c698014cac3429126fd7
Tejas Birla <tejasbirla3@gmail.com>
2025-11-06 12:32:11 +0530

    Third part (resend email configuration and setup)

--- backend/utils/sendMail.js
+++ backend/utils/sendMail.js
@@ -1,38 +1,20 @@
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
 import dotenv from "dotenv";
 dotenv.config();
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASSWORD,
-  },
-});
-
-transporter.verify((err, success) => {
-  if (err) {
-    console.error("Error verifying SMTP connection:", err);
-  } else {
-    console.log("SMTP connection verified and ready to send emails.");
-  }
-});
+const resend = new Resend(process.env.RESEND_EMAIL_API);
 
 const sendMail = (toEmail, subject, text) => {
-  const mailOptions = {
-    from: process.env.EMAIL_USER,
-    to: toEmail,
-    subject,
-    text,
-  };
-
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log("Email not send,an error occur: ", error);
-    } else {
-      console.log("Email sent", info.response);
-    }
-  });
+  try {
+     resend.emails.send({
+      from: process.env.EMAIL_USER,
+      to: toEmail,
+      subject,
+      text,
+    });
+  } catch (error) {
+    console.log("Error in sending mail: ", error);
+  }
 };
 
 export default sendMail;
